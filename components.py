84f5cc951b9d7c6b87a934cb89473d6dbe67fe56
Poehee <bartwww@gmail.com>
2023-01-07 17:23:33 +0100

    Fiddling, nothing substantial yet

--- components.py
+++ components.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from enum import Enum
+from typing import Any
 from settings import config
 
 class Color(Enum):
@@ -18,21 +20,25 @@ class Chips:
     bag=[]
 
     def __init__(self) -> None:
-        # Read about *: https://stackoverflow.com/questions/1720421/how-do-i-concatenate-two-lists-in-python
+        # Read about unpack with *: https://stackoverflow.com/questions/1720421/how-do-i-concatenate-two-lists-in-python
+        # Many ways to join two lists: https://www.digitalocean.com/community/tutorials/concatenate-lists-python
         for color in Color:
-            self.bag += [color.value for i in range(config.chipcount.per_color)]
+            self.bag += [color.value] * config.chipcount.color
 
-    def x__str__(self):
-        return str(self.bag)
+        self.bag += [SpecialChip.WILD.value] * config.chipcount.wilds
+        self.bag += [SpecialChip.RARE.value] * config.chipcount.rares
 
-    #bag = [chip 
-    matrix = [[0 for i in range(config.constants.board_size)] for j in range(config.constants.board_size)]
+class Board:
+    squares = [[None for i in range(config.constants.board_size)] for j in range(config.constants.board_size)]
+
+class Bag:
     pass
 
 if __name__ == "__main__":
-    print(Color(3))         #Color.BLUE
-    print(SpecialChip.GOLD.value) #1
-    print(type (Color.RED)) #<enum 'Color'>
-    print(SpecialChip.WILD)       #GOLD
-    print(Chips().matrix)
+    print(Color(3))                # Color.BLUE
+    print(SpecialChip.GOLD.value)  # 6
+    print(type (Color.RED))        # <enum 'Color'>
+    print(SpecialChip.WILD)        # SpecialChip.GOLD
+    print(Chips().bag)
+    print(Board().squares)
     print(Chips())
