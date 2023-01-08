feed238c01d29103258bc9ff80ea0af22d049692
Poehee <bartwww@gmail.com>
2023-01-08 18:31:54 +0100

    Filling bag randomized and drawing from it

--- components.py
+++ components.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Any
 from settings import config
+import random
 
 class Color(Enum):
     RED=1
@@ -17,28 +18,54 @@ class SpecialChip(Enum):
     RARE=PINK    
 
 class Chips:
-    bag=[]
-
     def __init__(self) -> None:
+        self.complete_set = []
         # Read about unpack with *: https://stackoverflow.com/questions/1720421/how-do-i-concatenate-two-lists-in-python
         # Many ways to join two lists: https://www.digitalocean.com/community/tutorials/concatenate-lists-python
         for color in Color:
-            self.bag += [color.value] * config.chipcount.color
+            self.complete_set += [color.value] * config.chipcount.color
 
-        self.bag += [SpecialChip.WILD.value] * config.chipcount.wilds
-        self.bag += [SpecialChip.RARE.value] * config.chipcount.rares
-
-class Board:
-    squares = [[None for i in range(config.constants.board_size)] for j in range(config.constants.board_size)]
+        self.complete_set += [SpecialChip.WILD.value] * config.chipcount.wilds
+        self.complete_set += [SpecialChip.RARE.value] * config.chipcount.rares
+        
+        random.shuffle(self.complete_set)
 
 class Bag:
-    pass
+    def __init__(self) -> None:
+        self.chips = Chips().complete_set #When bag is instantiated, directly fill it with all the chips
+
+#    def fill(self, chips: Chips):
+#        self.chips = chips
+
+    def draw_chip(self) -> int:
+        if len(self.chips) == 0:
+            return None
+        return self.chips.pop(0)
+
+class Board:
+    # Using (in)comprehension: https://www.geeksforgeeks.org/nested-list-comprehensions-in-python/
+    squares = [[None for i in range(config.constants.board_size)] for j in range(config.constants.board_size)]
+    def fill(self, bag: Bag) -> None:
+        pass
 
 if __name__ == "__main__":
     print(Color(3))                # Color.BLUE
     print(SpecialChip.GOLD.value)  # 6
     print(type (Color.RED))        # <enum 'Color'>
     print(SpecialChip.WILD)        # SpecialChip.GOLD
-    print(Chips().bag)
+    print(Chips().complete_set)
     print(Board().squares)
     print(Chips())
+
+    bag = Bag()
+    print(bag.chips)
+
+    for _ in range(len(bag.chips)):
+        print(bag.draw_chip())
+
+    print("Empty list....")  
+    for _ in range(len(bag.chips)):
+        print(bag.draw_chip())
+
+    print("Now what, no more chips - what happens when we draw?")
+    print(bag.draw_chip())
